f977518f0acddf1ad084b180d5b6b3d568568f0f
Ismael <ismaelrmzhdz@gmail.com>
2026-05-19 10:43:04 -0600

    Creacion datos falsos con seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create(['email' => 'ismvrr@test.com']);
+        
+        \App\Models\User::factory(19)->hasPosts(4)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // ...
     }
 }

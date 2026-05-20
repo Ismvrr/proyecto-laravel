beb087ac0c5d6c92ba9c3283da5f76befa3e9476
Ismael <ismaelrmzhdz@gmail.com>
2026-05-20 17:59:48 -0600

    Solicitud de amistad

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\FriendController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::post('/friends/{user}', [FriendController::class, 'store'])->name('friends.store');
 
     Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
     Route::get('/profile/{user}', [PageController::class, 'profile'])->name('profile.show');
